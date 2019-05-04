366972eb0bc270138206a8fb8c4020fb61e915dd
Chris Jones <chrisjonesdesign85@gmail.com>
2019-05-03 23:59:00 -0400

    just a few things left

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert("Hello World!")
+document.querySelector('body > header > div > h1').addEventListener("onload", fadeFunction)
+
+function fadeFunction() {
+    $('body').fadeIn(500);
+
+}
+
+
+document.querySelector('body > header').addEventListener()
+
+
+
+//toggle drop down 
+function myFunction() {
+    let x = document.querySelector('body > div.intro > div')
+    if (x.style.display === "block") {
+        x.style.display = "none"
+    } else {
+        x.style.display = "block"
+    }
+}
+
+//mouse over the drop down button
+function mOver(obj) {
+    obj.innerHTML = "Drop Down on Click"
+}
+
+function mOut(obj) {
+    obj.innerHTML = "Mouse Over Me"
+}
